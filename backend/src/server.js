32c6df48de3cebf5eef460bf441fc538cfc306d3
Jarrar7 <100485131+Jarrar7@users.noreply.github.com>
2024-07-28 20:19:01 +0300

    mongoDB Atlas

--- backend/src/server.js
+++ backend/src/server.js
@@ -19,11 +19,10 @@ app.use('/api/crypto', cryptoRoutes); // Use the new routes
 console.log('MongoDB URI:', process.env.MONGO_URI);
 
 // Connect to MongoDB using the environment variable
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log('MongoDB connection error:', err));
 
-// Use the port provided by Vercel's environment or default to 5000
+// Use the port provided by Vercel's environment
 const PORT = process.env.PORT || 5000;
-console.log(`Server will run on port ${PORT}`); // Log the port number
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
