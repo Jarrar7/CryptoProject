6d29448f8882659077a6b89acf723bd2af2ffb8d
Jarrar7 <100485131+Jarrar7@users.noreply.github.com>
2024-07-28 13:55:25 +0300

    .1.1

--- git_set.bat
+++
@@ -1,7 +0,0 @@
-echo "# CryptoProject" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/Jarrar7/CryptoProject.git
-git push -u origin main
